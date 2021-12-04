4fee94205877e53044b3fda08f2a6d49cb88a6bb
lenryk <lenry101@gmail.com>
2021-12-04 00:55:32 +0000

    solution to problem 19

--- 19-counting-sundays/solution.py
+++ 19-counting-sundays/solution.py
@@ -1,31 +1,14 @@
 import datetime
 
 start = datetime.date(1901, 1, 1)
-test = datetime.date(1901, 1, 5)
 END = datetime.date(2000, 12, 31)
-WEEK_DAYS=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 
-print(END - start)
-
-leap_years = list()
-
-for year in range(1901, 2001):
-  if (year % 4 == 0):
-    leap_years.append(year)
-
-print(leap_years)
-
-
-new_date = start + datetime.timedelta(days=7)
-print(new_date)
-
-print(start.weekday())
-print(start)
-
-while start.date() is not 1901-1-5:
+counter = 0
+while start != END:
   start += datetime.timedelta(days=1)
-print(start)
 
+  if start.weekday() == 6:
+    if start.strftime("%d") == "01":
+      counter += 1
 
-
-  
+print(counter)
